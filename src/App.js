3116159d2d2b781f6eafcd096b2e027ea1b5dc83
mayur <mp326395@gmail.com>
2023-01-30 21:50:00 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      Hey sellStack
-    </div>
-  );
+  return <div className="App">Hey sellStack</div>;
 }
 
 export default App;
